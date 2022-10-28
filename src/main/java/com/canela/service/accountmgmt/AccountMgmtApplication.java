d37691e9cebb5caf9e37f1b6a7b0ca8cdd581d26
David Ramirez <ramirezmonroyd@gmail.com>
2022-10-28 07:33:21 -0500

    Create Spring Boot project

---
+++ src/main/java/com/canela/service/accountmgmt/AccountMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.canela.service.accountmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountMgmtApplication.class, args);
+	}
+
+}
