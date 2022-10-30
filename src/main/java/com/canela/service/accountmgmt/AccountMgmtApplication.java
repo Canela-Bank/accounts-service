8b48a1ab45c58adf4a43bc45af820f437251c1c7
David Ramirez <ramirezmonroyd@gmail.com>
2022-10-30 18:18:41 -0500

    Add Swagger UI to project

--- src/main/java/com/canela/service/accountmgmt/AccountMgmtApplication.java
+++ src/main/java/com/canela/service/accountmgmt/AccountMgmtApplication.java
@@ -1,7 +1,12 @@
 package com.canela.service.accountmgmt;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AccountMgmtApplication {
@@ -10,4 +15,9 @@ public class AccountMgmtApplication {
 		SpringApplication.run(AccountMgmtApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI customOpenAPI(@Value("${application.name}") String appName,
+								 @Value("${application.description}") String description) {
+		return new OpenAPI().components(new Components()).info(new Info().title(appName).description(description));
+	}
 }
