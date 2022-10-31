84b7daeeac4544c6b924e150e89cdf256d409848
David Ramirez <ramirezmonroyd@gmail.com>
2022-10-30 19:58:03 -0500

    Update Swagger docs

--- src/main/java/com/canela/service/accountmgmt/controllers/pse/AddFundsController.java
+++ src/main/java/com/canela/service/accountmgmt/controllers/pse/AddFundsController.java
@@ -1,6 +1,8 @@
 package com.canela.service.accountmgmt.controllers.pse;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.PatternProperties;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
@@ -14,12 +16,14 @@ public class AddFundsController {
     @Operation(summary = "Add funds via PSE", description = "Add funds to the selected account from another bank via PSE", tags = {"Account"})
     @PostMapping(value = "add-funds/pse/{accountId}")
     @CrossOrigin("*")
-    public ResponseEntity<String> addFunds(@PathVariable(value = "accountId") String id, @RequestBody PseRequest req) {
+    public ResponseEntity<String> addFunds(@PathVariable(value = "accountId")
+                                           @Parameter(name = "Amount id", description = "Number of the account that will be updated", example = "33023227") String id,
+                                           @RequestBody PseRequest req) {
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(null);
     }
 
-    static class PseRequest{
-        @Schema(description = "Amount to add", example = "100000")
+    static class PseRequest {
+        @Schema(name = "Amount", description = "Amount to add", example = "100000")
         private Long amount;
 
         public Long getAmount() {
