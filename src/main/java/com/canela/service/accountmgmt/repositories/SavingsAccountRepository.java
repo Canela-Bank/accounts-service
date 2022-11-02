1cad28979309321a3a3cea099856295c787584d8
Camilo García <candres-garcia@javeriana.edu.co>
2022-11-01 23:16:09 -0500

    Create savings account entity.

--- src/main/java/com/canela/service/accountmgmt/repositories/SavingsAccountRepository.java
+++ src/main/java/com/canela/service/accountmgmt/repositories/SavingsAccountRepository.java
@@ -3,5 +3,5 @@ package com.canela.service.accountmgmt.repositories;
 import com.canela.service.accountmgmt.Entities.SavingsAccountEntity;
 import org.springframework.data.repository.CrudRepository;
 
-public interface SavingAccountRepository extends CrudRepository<SavingsAccountEntity, String> {
+public interface SavingsAccountRepository extends CrudRepository<SavingsAccountEntity, String> {
 }
