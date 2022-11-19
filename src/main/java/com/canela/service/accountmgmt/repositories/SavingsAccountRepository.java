8a9ddcad2c601dadbd2068a7a883e7e40200f87b
Sebastian Molano <smolanof@javeriana.edu.co>
2022-11-19 14:25:23 -0500

    Added Fixes, removed bugs and unnecessary code

--- src/main/java/com/canela/service/accountmgmt/repositories/SavingsAccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.canela.service.accountmgmt.repositories;
-
-import com.canela.service.accountmgmt.Entities.SavingsAccountEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface SavingsAccountRepository extends CrudRepository<SavingsAccountEntity, String> {
-}
