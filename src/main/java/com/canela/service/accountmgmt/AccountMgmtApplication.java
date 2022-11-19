2b868541b3dc8299925635a1cf498f44856b8c23
Carlos Loreto <sa_carlos@javeriana.edu.co>
2022-11-18 23:43:09 -0500

    Fix bug

--- src/main/java/com/canela/service/accountmgmt/AccountMgmtApplication.java
+++ src/main/java/com/canela/service/accountmgmt/AccountMgmtApplication.java
@@ -2,8 +2,9 @@ package com.canela.service.accountmgmt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class AccountMgmtApplication {
 
     public static void main(String[] args) {
