0a932763884b00f22314a5e1c84b639b54cc890d
Sebastian Molano <smolanof@javeriana.edu.co>
2022-11-20 13:12:40 -0500

    Se arreglo el deleteAccount

--- src/main/java/com/canela/service/accountmgmt/controllers/DeleteAccount.java
+++ src/main/java/com/canela/service/accountmgmt/controllers/DeleteAccount.java
@@ -1,16 +1,25 @@
 package com.canela.service.accountmgmt.controllers;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/accounts")
@@ -18,13 +27,22 @@ public class DeleteAccount {
 
     @DeleteMapping("/delete/{account}")
     public ResponseEntity<String> delete(@PathVariable String account){
-        URL url = null;
+        String url = "http://localhost:3002/graphql";
         try {
-            url = new URL("http://localhost:3002/graphql?query=mutation%7B%0A%20%20deleteAccount(id%"+account+"A%20%222%22)%7B%0A%20%20%20%20message%2C%0A%20%20%20%20data%20%7B%0A%20%20%20%20%20%20id%2C%0A%20%20%20%20%20%20balance%2C%0A%20%20%20%20%20%20user_id%2C%0A%20%20%20%20%20%20user_document_type%0A%20%20%20%20%7D%0A%20%20%7D%0A%7D%0A");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("POST");
-            int response = conn.getResponseCode();
-            if(response == HttpURLConnection.HTTP_OK){
+            CloseableHttpClient client = HttpClientBuilder.create().build();
+            HttpPost requestGraphQL = new HttpPost(url);
+            String query = String.format("mutation {\n" +
+                    "  deleteAccount(id: \"%s\") {\n" +
+                    "    message\n" +
+                    "  }\n" +
+                    "}", account);
+            URI uri = new URIBuilder(requestGraphQL.getURI())
+                    .addParameter("query", query)
+                    .build();
+            requestGraphQL.setURI(uri);
+            HttpResponse full_response = client.execute(requestGraphQL);
+            int response = full_response.getStatusLine().getStatusCode();
+            if(response == HttpStatus.OK.value()){
                 return ResponseEntity.status(HttpStatus.OK).body("Eliminado");
             }
         } catch (MalformedURLException e) {
